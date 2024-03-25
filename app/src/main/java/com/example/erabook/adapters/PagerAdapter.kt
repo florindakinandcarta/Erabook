4ac22b1476f96009969e628f6e071be1a97bb5e6
Florinda Hasani <florinda.hasani@kinandcarta.com>
2024-03-25 15:27:36 +0100

    tab layout, view pager  in home fragment

---
+++ app/src/main/java/com/example/erabook/adapters/PagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.erabook.adapters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.erabook.fragments.home.ObjectFragment
+import com.example.erabook.util.ARG_TAB_NAME
+import com.example.erabook.util.TAB_NAMES
+
+class PagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int {
+        return TAB_NAMES.size
+    }
+    override fun createFragment(position: Int): Fragment {
+        val fragment = ObjectFragment()
+        fragment.arguments = Bundle().apply {
+            putString(ARG_TAB_NAME, TAB_NAMES[position].type.name)
+        }
+        return fragment
+    }
+}
---
+++ app/src/main/java/com/example/erabook/fragments/home/ObjectFragment.kt
@@ -0,0 +1,81 @@
+package com.example.erabook.fragments.home
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.GestureDetector
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.erabook.R
+import com.example.erabook.databinding.FragmentObjectBinding
+import com.example.erabook.util.ARG_TAB_NAME
+import com.example.erabook.util.TAB_NAMES
+import com.example.erabook.util.showToast
+import com.google.android.material.tabs.TabLayout
+
+class ObjectFragment : Fragment() {
+    private lateinit var binding: FragmentObjectBinding
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager2
+    private lateinit var gestureDetector: GestureDetector
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentObjectBinding.inflate(layoutInflater, container, false)
+        viewPager = requireActivity().findViewById(R.id.pager)
+        tabLayout = requireActivity().findViewById(R.id.tab_layout)
+
+        gestureDetector =
+            GestureDetector(requireContext(), object : GestureDetector.SimpleOnGestureListener() {
+               override fun onFling(
+                    e1: MotionEvent?,
+                    e2: MotionEvent,
+                    velocityX: Float,
+                    velocityY: Float
+                ): Boolean {
+                    val sensitivity = 50
+                   e1?.let {
+                       if (e1.x - e2.x > sensitivity) {
+                           val nextTab = tabLayout.selectedTabPosition + 1
+                           if (nextTab < tabLayout.tabCount) {
+                               viewPager.currentItem = nextTab
+                           }
+                           return true
+                       } else if (e2.x - e1.x > sensitivity) {
+                           val previousTab = tabLayout.selectedTabPosition - 1
+                           if (previousTab >= 0) {
+                               viewPager.currentItem = previousTab
+                           }
+                           return true
+                       }
+                   }
+                    return false
+                }
+            })
+        binding.root.setOnTouchListener { _, event ->
+            gestureDetector.onTouchEvent(event)
+        }
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let { args ->
+            val tabName = args.getString(ARG_TAB_NAME)
+            val position = TAB_NAMES.indexOfFirst { it.type.name == tabName }
+            if (position != -1) {
+                binding.sampleText.text = getString(TAB_NAMES[position].tabName)
+            }
+        }
+        binding.googleImage.setOnClickListener {
+            requireContext().showToast(R.string.clicked)
+        }
+    }
+}
