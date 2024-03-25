fcaf13c3c68a3382a8d896cbc36cee75320ac8ee
Florinda Hasani <florinda.hasani@kinandcarta.com>
2024-03-25 16:08:39 +0100

    delete unused code on home fragment

--- app/src/main/java/com/example/erabook/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/erabook/fragments/home/HomeFragment.kt
@@ -1,15 +1,11 @@
 package com.example.erabook.fragments.home
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
-import com.example.erabook.R
-import com.example.erabook.adapters.HomeAdapter
 import com.example.erabook.adapters.PagerAdapter
 import com.example.erabook.databinding.FragmentHomeBinding
 import com.example.erabook.util.TAB_NAMES
@@ -18,17 +14,8 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
-    private lateinit var homeAdapter: HomeAdapter
-    private lateinit var homeViewModel: HomeViewModel
     private lateinit var viewPager: ViewPager2
     private lateinit var demoCollectionPagerAdapter: PagerAdapter
-
-    companion object {
-        lateinit var sharedPreferences: SharedPreferences
-            private set
-    }
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
 
 
     override fun onCreateView(
@@ -52,37 +39,5 @@ class HomeFragment : Fragment() {
             tab.text = getString(TAB_NAMES[position].tabName)
         }.attach()
         tabLayout.tabMode = TabLayout.MODE_SCROLLABLE
-//        binding.apply {
-//            homeListRecycleView.apply {
-//                adapter = homeAdapter
-//            }
-//        }
-//        homeViewModel.booksList.observe(viewLifecycleOwner) { listOfBooks ->
-//            homeAdapter.submitList(listOfBooks)
-//        }
-        binding.topAppBar.setOnMenuItemClickListener {
-            when (it.itemId) {
-                R.id.searchFragmentIcon -> {
-                    findNavController().navigate(R.id.homeToSearch)
-                    true
-                }
-
-                else -> false
-            }
-        }
     }
 }
-
-//    }
-//        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
-//
-//        )[HomeViewModel::class.java]
-//                .bufferedReader().use { it.readText() })
-//            this, HomeViewModelFactory(requireContext().assets.open("books.json")
-//        homeViewModel = ViewModelProvider(
-//
-//        }
-//            homeViewModel.addToFavorites(clickedBook)
-//        homeAdapter = HomeAdapter { clickedBook ->
-//        super.onCreate(savedInstanceState)
-
