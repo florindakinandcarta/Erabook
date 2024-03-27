9ce394b08044f4c15c7e071fbce57d203f77f7ab
Florinda Hasani <florinda.hasani@kinandcarta.com>
2024-03-27 19:26:33 +0100

    tried something with these don't need them now

--- app/src/main/java/com/example/erabook/data/models/nyt/BookItem.kt
+++
@@ -1,5 +0,0 @@
-package com.example.erabook.data.models.nyt
-
-import com.example.erabook.util.BookCategory
-
-data class BookItem(val category: BookCategory, val books: Books)
--- app/src/main/java/com/example/erabook/util/BookCategory.kt
+++
@@ -1,9 +0,0 @@
-package com.example.erabook.util
-
-enum class BookCategory {
-    PAPERBACK_NONFICTION,
-    SERIES,
-    TRADE_FICTION,
-    ADVICE_HOW_TO,
-    PAPERBACK_GRAPHIC
-}
