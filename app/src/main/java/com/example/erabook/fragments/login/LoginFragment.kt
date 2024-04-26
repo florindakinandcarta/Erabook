d464caed6ea5c02f40709351230b50f8226ce6aa
Florinda Hasani <florinda.hasani@kinandcarta.com>
2024-04-26 11:00:43 +0200

    checking empty fields for email and password

--- app/src/main/java/com/example/erabook/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/example/erabook/fragments/login/LoginFragment.kt
@@ -12,9 +12,9 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.example.erabook.R
 import com.example.erabook.activities.AuthenticationViewModel
 import com.example.erabook.activities.MainActivity
-import com.example.erabook.R
 import com.example.erabook.data.models.UserDataRemote
 import com.example.erabook.databinding.FragmentLogInBinding
 import com.example.erabook.firebaseActivities.ForgotPasswordActivity
@@ -25,120 +25,103 @@ import com.google.android.gms.common.api.ApiException
 private const val DESTINATION_HOME = "HOME_FRAGMENT"
 
 class LoginFragment : Fragment() {
-    private lateinit var binding: FragmentLogInBinding
-    private val authenticationViewModel: AuthenticationViewModel by viewModels()
-    private val userInfoViewModel: UserInfoViewModel by viewModels()
-    private lateinit var startSignInActivityForResult: ActivityResultLauncher<Intent>
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentLogInBinding.inflate(layoutInflater, container, false)
-        return binding.root
+  private lateinit var binding: FragmentLogInBinding
+  private val authenticationViewModel: AuthenticationViewModel by viewModels()
+  private val userInfoViewModel: UserInfoViewModel by viewModels()
+  private lateinit var startSignInActivityForResult: ActivityResultLauncher<Intent>
+
+  override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+  ): View {
+    binding = FragmentLogInBinding.inflate(layoutInflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    authenticationViewModel.userLiveData.observe(viewLifecycleOwner) { user ->
+      if (user != null) {
+        findNavController().navigate(R.id.loginToProfile)
+      }
     }
+    authenticationViewModel.googleSignInOptions(requireContext())
+    setupOnClickListeners()
 
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        authenticationViewModel.userLiveData.observe(viewLifecycleOwner) { user ->
-            if (user != null) {
-                findNavController().navigate(R.id.loginToProfile)
-            }
+    startSignInActivityForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+          if (result.resultCode == RESULT_OK) {
+            val data: Intent? = result.data
+            handleSignInResult(data)
+          }
         }
-        authenticationViewModel.googleSignInOptions(requireContext())
-        setupOnClickListeners()
+  }
 
-        startSignInActivityForResult =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == RESULT_OK) {
-                    val data: Intent? = result.data
-                    handleSignInResult(data)
-                }
-
-            }
-    }
-
-    private fun handleSignInResult(data: Intent?) {
-        try {
-            val account =
-                GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException::class.java)
-            authenticationViewModel.firebaseAuthWithGoogle(
-                account?.idToken,
-                {
-                    UserDataRemote(
-                        userEmail = account?.email,
-                    ).let {
-                        userInfoViewModel.addUserData(it)
-                    }
-                    requireContext().showToast(R.string.login_successful)
-                },
-                {
-                    requireContext().showToast(R.string.auth_failed)
-                }
-            )
-        } catch (e: ApiException) {
-            println("Error: $e")
-        }
-    }
-
-
-    private fun setupOnClickListeners() {
-        binding.apply {
-            goToRegister.setOnClickListener {
-                findNavController().navigate(R.id.loginToRegister)
-            }
-            backLogin.setOnClickListener {
-                val intent = Intent(requireContext(), MainActivity::class.java).apply {
-                    putExtra(DESTINATION_HOME, R.id.homeFragment)
-                }
-                startActivity(intent)
-            }
-            googleButton.setOnClickListener {
-                authenticationViewModel.signOut()
-                val signInIntent = authenticationViewModel.getGoogleSignInIntent()
-                signInIntent?.let { intent ->
-                    startSignInActivityForResult.launch(intent)
-                }
-            }
-            loginButton.setOnClickListener {
-                val emailLogIn = emailLoginInput.editText?.text.toString()
-                val passwordLogIn = passwordLoginInput.editText?.text.toString()
-                if (emailLogIn.isEmpty()) {
-                    requireContext().showToast(R.string.enter_email)
-                }
-                if (passwordLogIn.isEmpty()) {
-                    requireContext().showToast(R.string.enter_password)
-                }
-                authenticationViewModel.signInWithEmailAndPassword(emailLogIn.trim(), passwordLogIn)
-                authenticationViewModel.isTaskSuccessful.observe(viewLifecycleOwner) { task ->
-                    if (task == true) {
-                        findNavController().navigate(R.id.loginToProfile)
-                        requireContext().showToast(R.string.login_successful)
-                    } else {
-                        authenticationViewModel.exception.observe(viewLifecycleOwner) { message ->
-                            when (message) {
-                                null -> requireContext().showToast(R.string.default_error)
-                                getString(R.string.error_code_message_incorrect) -> requireContext().showToast(
-                                    R.string.wrong_password_email
-                                )
-
-                                getString(R.string.error_code_message_connection) -> requireContext().showToast(
-                                    R.string.error_connection
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-            forgotPasswordTextView.setOnClickListener {
-                startActivity(
-                    Intent(
-                        requireContext(),
-                        ForgotPasswordActivity::class.java
-                    )
+  private fun handleSignInResult(data: Intent?) {
+    try {
+      val account =
+          GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException::class.java)
+      authenticationViewModel.firebaseAuthWithGoogle(
+          account?.idToken,
+          {
+            UserDataRemote(
+                    userEmail = account?.email,
                 )
-            }
-        }
+                .let { userInfoViewModel.addUserData(it) }
+            requireContext().showToast(R.string.login_successful)
+          },
+          { requireContext().showToast(R.string.auth_failed) })
+    } catch (e: ApiException) {
+      println("Error: $e")
     }
+  }
+
+  private fun setupOnClickListeners() {
+    binding.apply {
+      goToRegister.setOnClickListener { findNavController().navigate(R.id.loginToRegister) }
+      backLogin.setOnClickListener {
+        val intent =
+            Intent(requireContext(), MainActivity::class.java).apply {
+              putExtra(DESTINATION_HOME, R.id.homeFragment)
+            }
+        startActivity(intent)
+      }
+      googleButton.setOnClickListener {
+        authenticationViewModel.signOut()
+        val signInIntent = authenticationViewModel.getGoogleSignInIntent()
+        signInIntent?.let { intent -> startSignInActivityForResult.launch(intent) }
+      }
+      loginButton.setOnClickListener {
+        val emailLogIn = emailLoginInput.editText?.text.toString()
+        val passwordLogIn = passwordLoginInput.editText?.text.toString()
+        if (emailLogIn.isEmpty()) {
+          requireContext().showToast(R.string.enter_email)
+        } else if (passwordLogIn.isEmpty()) {
+          requireContext().showToast(R.string.enter_password)
+        } else {
+          authenticationViewModel.signInWithEmailAndPassword(emailLogIn.trim(), passwordLogIn)
+          authenticationViewModel.isTaskSuccessful.observe(viewLifecycleOwner) { task ->
+            if (task == true) {
+              findNavController().navigate(R.id.loginToProfile)
+              requireContext().showToast(R.string.login_successful)
+            } else {
+              authenticationViewModel.exception.observe(viewLifecycleOwner) { message ->
+                when (message) {
+                  null -> requireContext().showToast(R.string.default_error)
+                  getString(R.string.error_code_message_incorrect) ->
+                      requireContext().showToast(R.string.wrong_password_email)
+                  getString(R.string.error_code_message_connection) ->
+                      requireContext().showToast(R.string.error_connection)
+                }
+              }
+            }
+          }
+        }
+        forgotPasswordTextView.setOnClickListener {
+          startActivity(Intent(requireContext(), ForgotPasswordActivity::class.java))
+        }
+      }
+    }
+  }
 }
--- app/src/main/java/com/example/erabook/fragments/register/RegisterFragment.kt
+++ app/src/main/java/com/example/erabook/fragments/register/RegisterFragment.kt
@@ -18,90 +18,76 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment() {
-    private lateinit var binding: FragmentRegisterBinding
-    private lateinit var auth: FirebaseAuth
-    private val userInfoViewModel: UserInfoViewModel by viewModels()
+  private lateinit var binding: FragmentRegisterBinding
+  private lateinit var auth: FirebaseAuth
+  private val userInfoViewModel: UserInfoViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
-        auth = Firebase.auth
-        return binding.root
-    }
+  override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+  ): View {
+    binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+    auth = Firebase.auth
+    return binding.root
+  }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.apply {
-            emailInput.editText?.addTextChangedListener { editable ->
-                if (editable.toString().trim()
-                        .isNotEmpty() && !android.util.Patterns.EMAIL_ADDRESS.matcher(
-                        editable.toString().trim()
-                    )
-                        .matches()
-                ) {
-                    emailMessage.visibility = View.VISIBLE
-                } else {
-                    emailMessage.visibility = View.GONE
-                }
-            }
-            confirmPasswordInput.editText?.addTextChangedListener { passwordEditable ->
-                if (passwordInput.editText?.text.toString().trim() != passwordEditable.toString()
-                        .trim()
-                ) {
-                    passwordMessage.visibility = View.VISIBLE
-                } else {
-                    passwordMessage.visibility = View.GONE
-                }
-            }
-            setupOnClickListeners()
-
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    binding.apply {
+      emailInput.editText?.addTextChangedListener { editable ->
+        if (editable.toString().trim().isNotEmpty() &&
+            !android.util.Patterns.EMAIL_ADDRESS.matcher(editable.toString().trim()).matches()) {
+          emailMessage.visibility = View.VISIBLE
+        } else {
+          emailMessage.visibility = View.GONE
         }
-    }
-
-
-    private fun setupOnClickListeners() {
-        binding.apply {
-            registerButton.setOnClickListener {
-                val email = emailInput.editText?.text.toString()
-                val password = passwordInput.editText?.text.toString()
-
-                if (email.isEmpty()) {
-                    requireContext().showToast(R.string.enter_email)
-                    return@setOnClickListener
-                }
-                if (password.isEmpty()) {
-                    requireContext().showToast(R.string.enter_password)
-                    return@setOnClickListener
-                }
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            UserDataRemote(
-                                userEmail = email,
-                            ).let {
-                                userInfoViewModel.addUserData(it)
-                            }
-                            emailInput.editText?.setText("")
-                            passwordInput.editText?.setText("")
-                            usernameInput.editText?.setText("")
-                            confirmPasswordInput.editText?.setText("")
-                            requireActivity().showToast(R.string.signup_succ)
-                        } else if (password.length < 6) {
-                            requireContext().showToast(R.string.password_to_short)
-                        } else {
-                            requireContext().showToast(R.string.auth_failed)
-                        }
-                    }
-            }
-            alreadyHaveLoginTextview.setOnClickListener {
-                findNavController().navigate(R.id.registerToLogin)
-            }
-            backRegister.setOnClickListener {
-                requireActivity().onBackPressedDispatcher.onBackPressed()
-            }
+      }
+      confirmPasswordInput.editText?.addTextChangedListener { passwordEditable ->
+        if (passwordInput.editText?.text.toString().trim() != passwordEditable.toString().trim()) {
+          passwordMessage.visibility = View.VISIBLE
+        } else {
+          passwordMessage.visibility = View.GONE
         }
+      }
+      setupOnClickListeners()
     }
+  }
+
+  private fun setupOnClickListeners() {
+    binding.apply {
+      registerButton.setOnClickListener {
+        val email = emailInput.editText?.text.toString()
+        val password = passwordInput.editText?.text.toString()
+
+        if (email.isEmpty()) {
+          requireContext().showToast(R.string.enter_email)
+        } else if (password.isEmpty()) {
+          requireContext().showToast(R.string.enter_password)
+        } else {
+          auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+              UserDataRemote(
+                      userEmail = email,
+                  )
+                  .let { userInfoViewModel.addUserData(it) }
+              emailInput.editText?.setText("")
+              passwordInput.editText?.setText("")
+              usernameInput.editText?.setText("")
+              confirmPasswordInput.editText?.setText("")
+              requireActivity().showToast(R.string.signup_succ)
+            } else if (password.length < 6) {
+              requireContext().showToast(R.string.password_to_short)
+            } else {
+              requireContext().showToast(R.string.auth_failed)
+            }
+          }
+        }
+      }
+      alreadyHaveLoginTextview.setOnClickListener {
+        findNavController().navigate(R.id.registerToLogin)
+      }
+      backRegister.setOnClickListener { requireActivity().onBackPressedDispatcher.onBackPressed() }
+    }
+  }
 }
